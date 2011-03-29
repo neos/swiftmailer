eac972b454c190dfa5819f170c35a7b359fa4c0f
Christopher Hlubek <hlubek@networkteam.com>
2011-03-29 12:39:11 +0200

    [+FEATURE] Add dummy mail transport with logging
    This change introduces a special transport that logs and stores
    sent messages. It is useful for functional tests and during
    development to see sent messages directly in the log.
    
    Change-Id: I0d3005974582229cc20bcdba1651bb6db74846ea

--- Classes/Message.php
+++ Classes/Message.php
@@ -81,5 +81,4 @@ class Message extends \Swift_Message {
 	}
 
 }
-
 ?>
---
+++ Classes/Transport/LoggingTransport.php
@@ -0,0 +1,121 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\SwiftMailer\Transport;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "SwiftMailer".                *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * A logging swift transport for functional tests and development. It stores and logs sent messages.
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License', version 3 or later
+ */
+class LoggingTransport implements \F3\SwiftMailer\TransportInterface {
+
+	/**
+	 * Store sent messages for testing
+	 *
+	 * @var array
+	 */
+	static protected $deliveredMessages = array();
+
+	/**
+	 * @inject
+	 * @var \F3\FLOW3\Log\SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * The logging transport is always started
+	 *
+	 * @return boolean Always TRUE for this transport
+	 */
+	public function isStarted() {
+		return TRUE;
+	}
+
+	/**
+	 * No op
+	 *
+	 * @return void
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function start() {}
+
+	/**
+	 * No op
+	 *
+	 * @return void
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function stop() {}
+
+	/**
+	 * "Send" the given Message. This transport will add it to a stored collection of sent messages
+	 * for testing purposes and log the message to the system logger.
+	 *
+	 * @param \Swift_Mime_Message $message The message to send
+	 * @param array &$failedRecipients Failed recipients
+	 * @return int
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function send(\Swift_Mime_Message $message, &$failedRecipients = NULL) {
+		$count = count((array)$message->getTo()) + count((array)$message->getCc()) + count((array)$message->getBcc());
+
+		self::$deliveredMessages[] = $message;
+
+		$this->systemLogger->log('Sent email to ' . $message->getTo(), LOG_DEBUG, array(
+			'message' => $message->toString()
+		));
+
+		return $count;
+	}
+
+	/**
+	 * No op
+	 *
+	 * @param \Swift_Events_EventListener $plugin
+	 * @return void
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function registerPlugin(\Swift_Events_EventListener $plugin) {}
+
+	/**
+	 * Get delivered messages that were sent through this transport
+	 *
+	 * @return array
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	static public function getDeliveredMessages() {
+		return self::$deliveredMessages;
+	}
+
+	/**
+	 * Reset the delivered messages (e.g. for tearDown in functional test)
+	 *
+	 * @return void
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	static public function reset() {
+		self::$deliveredMessages = array();
+	}
+
+}
+?>

f3456b1eb07ce54a76c20551b89bf1146a24e296
Karsten Dambekalns <karsten@neos.io>
2015-06-22 12:52:04 +0200

    [TASK] Add methods to MailerInterface so the contract is clear
    Change-Id: I5fc8ed8f2f41f1a3ec5760a45cb755554020d0f1

--- Classes/TYPO3/SwiftMailer/MailerInterface.php
+++ Classes/TYPO3/SwiftMailer/MailerInterface.php
@@ -15,4 +15,36 @@ namespace TYPO3\SwiftMailer;
  * Mailer interface for the SwiftMailer package
  */
 interface MailerInterface {
+
+	/**
+	 * Send the given Message like it would be sent in a mail client.
+	 *
+	 * All recipients (with the exception of Bcc) will be able to see the other
+	 * recipients this message was sent to.
+	 *
+	 * Recipient/sender data will be retrieved from the Message object.
+	 *
+	 * The return value is the number of recipients who were accepted for
+	 * delivery.
+	 *
+	 * @param \Swift_Mime_Message $message
+	 * @param array $failedRecipients An array of failures by-reference
+	 * @return int
+	 */
+	public function send(\Swift_Mime_Message $message, &$failedRecipients = null);
+
+	/**
+	 * Register a plugin using a known unique key (e.g. myPlugin).
+	 *
+	 * @param \Swift_Events_EventListener $plugin
+	 */
+	public function registerPlugin(\Swift_Events_EventListener $plugin);
+
+	/**
+	 * The Transport used to send messages.
+	 *
+	 * @return \Swift_Transport
+	 */
+	public function getTransport();
+
 }
